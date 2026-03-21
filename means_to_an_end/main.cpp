5595ab1da6de708f5dd2dfcab6f1e6819e389d1c
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-03-21 11:48:19 +0200

    Add means_to_an_end argument handling

--- means_to_an_end/main.cpp
+++ means_to_an_end/main.cpp
@@ -9,7 +9,22 @@ int main(int argc, char** argv) {
         server_address = "0.0.0.0";
         server_port = 54321;
     }
+    else if (argc != 3) {
+        std::println("[*] USAGE: ./server <IP> <PORT>");
+        return EXIT_FAILURE;
+    }
+    else {
+        server_address = argv[1];
+        int32_t temp = std::stoi(argv[2]);
+        if (temp <= static_cast<int>(UINT16_MAX) && temp >= 0)
+            server_port = static_cast<uint16_t>(temp);
+        else {
+            std::println("[-] Invalid port number");
+            return EXIT_FAILURE;
+        }
+    }
 
     std::println("[*] Binding to {}:{}", server_address, server_port);
+
     return EXIT_SUCCESS;
 }
